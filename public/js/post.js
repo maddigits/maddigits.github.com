e5312567a9878be1b480299a4d671119a97f834e
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-10-08 21:16:48 -0300

    Added links

---
+++ public/js/post.js
@@ -0,0 +1,29 @@
+(function () {
+  function getText(element) {
+    var value = element.nodeValue;
+    if (!value)
+      if (!element.firstChild)
+        return null;
+      else
+        return getText(element.firstChild);
+    return value.replace(/(\n|  |\t)/g, '');
+  }
+
+  function createLink(header, id) {
+    var link = document.createElement('a');
+    link.href = '#' + id;
+    link.innerText = '¶';
+    header.appendChild(link);
+  }
+
+  function execute(header) {
+    var id = getText(header.firstChild);
+    if (!id)
+      return;
+    header.id = id;
+    createLink(header, id);
+  }
+  document.addEventListener('DOMContentLoaded', function () {
+    [].forEach.call(document.querySelectorAll('.post h2, .post h3'), execute);
+  });
+})();
